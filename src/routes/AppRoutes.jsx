39c59aee5da8a20931124812b6f5cfb52381bc05
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2025-11-26 01:04:06 +0100

    Rotas de autenticação adicionadas

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -3,12 +3,22 @@ import { Routes, Route } from "react-router-dom";
 import Home from "../pages/site/Home";
 import NotFound from "../pages/site/NotFound";
 
+//Autenticação
+import Login from "../pages/auth/Login";
+import Register from "../pages/auth/Register";
+import ForgotPassword from "../pages/auth/ForgotPassword";
+
 export default function AppRoutes () {
     return (
         <Routes>
             {/*Rotas do site */}
             <Route path="/" element={<Home />} />
             <Route path="*" element={<NotFound />} />
+
+            {/*Rotas de autenticação */}
+            <Route path="/auth/login" element={<Login />} />
+            <Route path="/auth/register" element={<Register />} />
+            <Route path="/auth/forgot-password" element={<ForgotPassword />} />
         </Routes>
     )
 }
